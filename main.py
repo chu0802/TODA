00b69b1e3dc88d8c109b70070737e9d718a35843
b05902105 <b05902105@ntu.eud.tw>
2022-06-08 12:23:02 +0800

    check 3shot shot with source classifier, lr 0.1

--- main.py
+++ main.py
@@ -387,17 +387,17 @@ def main(args):
         c = Classifier(bottleneck_dim, args.dataset['num_classes']).cuda()
         
         # load(f'{args.dataset["name"]}/s{args.source}_{args.source + 2020}.pt', f=f, b=b, c=c)
-        # load(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.source + 2020}.pt', f=f, b=b, c=c)
+        load(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.source + 2020}_source_only.pt', f=f, b=b, c=c)
         
         
 
-        # for param in c.parameters():
-            # param.requires_grad = False
+        for param in c.parameters():
+            param.requires_grad = False
         
         params = [
             {'params': f.parameters(), 'base_lr': args.lr*0.1, 'lr': args.lr*0.1},
-            {'params': b.parameters(), 'base_lr': args.lr, 'lr': args.lr},
-            {'params': c.parameters(), 'base_lr': args.lr, 'lr': args.lr}
+            {'params': b.parameters(), 'base_lr': args.lr, 'lr': args.lr}
+            # {'params': c.parameters(), 'base_lr': args.lr, 'lr': args.lr}
         ]
         
         opt = torch.optim.SGD(params, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
@@ -445,8 +445,8 @@ def main(args):
             lx, ly = next(l_iter)
             lx, ly = lx.float().cuda(), ly.long().cuda()
             
-            sx, sy = next(s_iter)
-            sx, sy = sx.float().cuda(), sy.long().cuda()
+            # sx, sy = next(s_iter)
+            # sx, sy = sx.float().cuda(), sy.long().cuda()
             
             ux, _ = next(u_iter)
             ux = ux.float().cuda()
@@ -465,9 +465,9 @@ def main(args):
             
             opt.zero_grad()
             
-            inputs, targets = torch.cat((sx, lx)), torch.cat((sy, ly))
-            l_out = c(b(f(inputs)))
-            l_loss = criterion(l_out, targets)
+            # inputs, targets = torch.cat((sx, lx)), torch.cat((sy, ly))
+            l_out = c(b(f(lx)))
+            l_loss = criterion(l_out, ly)
             
             l_loss.backward()
             opt.step()
@@ -475,34 +475,34 @@ def main(args):
             # for param in c.parameters():
             #     param.requires_grad = True
             
-            opt.zero_grad()
-            
-            u_out = c(b(f(ux), reverse=True))
-            
-            soft_out = F.softmax(u_out, dim=1)
-            u_loss = args.lambda_u * torch.mean(torch.sum(soft_out * (torch.log(soft_out + 1e-5)), dim=1))
-            
-            u_loss.backward()
-            opt.step()
-
-            # u_out = c(b(f(ux)))
-
-            # softmax_out = F.softmax(u_out, dim=1)
-            # entropy = -softmax_out * torch.log(softmax_out + 1e-5)
-            # entropy = torch.sum(entropy, dim=1)
-
-            # ent_loss = torch.mean(entropy)
-
-            # msoftmax = softmax_out.mean(dim=0)
-            # gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + 1e-5))
-
-            # ent_loss -= gentropy_loss
-            
-            # loss = args.lambda_u * ent_loss
-
             # opt.zero_grad()
-            # loss.backward()
+            
+            # u_out = c(b(f(ux), reverse=True))
+            
+            # soft_out = F.softmax(u_out, dim=1)
+            # u_loss = args.lambda_u * torch.mean(torch.sum(soft_out * (torch.log(soft_out + 1e-5)), dim=1))
+            
+            # u_loss.backward()
             # opt.step()
+
+            u_out = c(b(f(ux)))
+
+            softmax_out = F.softmax(u_out, dim=1)
+            entropy = -softmax_out * torch.log(softmax_out + 1e-5)
+            entropy = torch.sum(entropy, dim=1)
+
+            ent_loss = torch.mean(entropy)
+
+            msoftmax = softmax_out.mean(dim=0)
+            gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + 1e-5))
+
+            ent_loss -= gentropy_loss
+            
+            loss = args.lambda_u * ent_loss
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
             
             lr_scheduler.step()
 
