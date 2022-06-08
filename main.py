8adafe82f84d750fa3b18cf3ff23d7689d959d64
b05902105 <b05902105@ntu.eud.tw>
2022-06-08 12:44:25 +0800

    end to end shot without gentorpy

--- main.py
+++ main.py
@@ -387,17 +387,17 @@ def main(args):
         c = Classifier(bottleneck_dim, args.dataset['num_classes']).cuda()
         
         # load(f'{args.dataset["name"]}/s{args.source}_{args.source + 2020}.pt', f=f, b=b, c=c)
-        load(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.source + 2020}.pt', f=f, b=b, c=c)
+        # load(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.source + 2020}.pt', f=f, b=b, c=c)
         
         
 
-        for param in c.parameters():
-            param.requires_grad = False
+        # for param in c.parameters():
+        #     param.requires_grad = False
         
         params = [
             {'params': f.parameters(), 'base_lr': args.lr*0.1, 'lr': args.lr*0.1},
-            {'params': b.parameters(), 'base_lr': args.lr, 'lr': args.lr}
-            # {'params': c.parameters(), 'base_lr': args.lr, 'lr': args.lr}
+            {'params': b.parameters(), 'base_lr': args.lr, 'lr': args.lr},
+            {'params': c.parameters(), 'base_lr': args.lr, 'lr': args.lr}
         ]
         
         opt = torch.optim.SGD(params, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
@@ -442,26 +442,26 @@ def main(args):
 
         for i in range(1, args.num_iters+1):
             print('iteration: %03d/%03d, lr: %.4f' % (i, args.num_iters, lr_scheduler.get_lr()), end='\r')
-            # lx, ly = next(l_iter)
-            # lx, ly = lx.float().cuda(), ly.long().cuda()
+            lx, ly = next(l_iter)
+            lx, ly = lx.float().cuda(), ly.long().cuda()
             
-            # sx, sy = next(s_iter)
-            # sx, sy = sx.float().cuda(), sy.long().cuda()
+            sx, sy = next(s_iter)
+            sx, sy = sx.float().cuda(), sy.long().cuda()
             
             ux, _ = next(u_iter)
             ux = ux.float().cuda()
 
-            # opt.zero_grad()
+            opt.zero_grad()
             
-            # # inputs, targets = torch.cat((sx, lx)), torch.cat((sy, ly))
-            # l_out = c(b(f(sx)))
-            # l_loss = criterion(l_out, sy)
+            inputs, targets = torch.cat((sx, lx)), torch.cat((sy, ly))
+            l_out = c(b(f(inputs)))
+            l_loss = criterion(l_out, targets)
             
-            # l_loss.backward()
-            # opt.step()
+            l_loss.backward()
+            opt.step()
 
-            # for param in c.parameters():
-            #     param.requires_grad = False
+            for param in c.parameters():
+                param.requires_grad = False
             
             # opt.zero_grad()
             
@@ -471,9 +471,6 @@ def main(args):
             
             # l_loss.backward()
             # opt.step()
-
-            # for param in c.parameters():
-            #     param.requires_grad = True
             
             # opt.zero_grad()
             
@@ -493,16 +490,19 @@ def main(args):
 
             ent_loss = torch.mean(entropy)
 
-            msoftmax = softmax_out.mean(dim=0)
-            gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + 1e-5))
+            # msoftmax = softmax_out.mean(dim=0)
+            # gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + 1e-5))
 
-            ent_loss -= gentropy_loss
+            # ent_loss -= gentropy_loss
             
             loss = args.lambda_u * ent_loss
 
             opt.zero_grad()
             loss.backward()
             opt.step()
+
+            for param in c.parameters():
+                param.requires_grad = True
             
             lr_scheduler.step()
 
