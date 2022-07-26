83282534e6f7993042caf3c62c17675bf5b9ca6a
b05902105 <b05902105@ntu.eud.tw>
2022-07-26 13:13:57 +0800

    soft loss for s

--- main.py
+++ main.py
@@ -516,7 +516,7 @@ def main(args):
             
             sx, sy = next(s_iter)
             sx, sy = sx.float().cuda(), sy.long().cuda()
-            # soft_sy = class_soft_labels[sy]
+            soft_sy = class_soft_labels[sy]
             ux, _ = next(u_iter)
             ux = ux.float().cuda()
 
@@ -528,11 +528,11 @@ def main(args):
             s_log_softmax_out = F.log_softmax(s_out, dim=1)
             l_loss = torch.nn.CrossEntropyLoss(reduction='none')(s_out, sy)
             
-            # soft_loss = -(soft_sy * s_log_softmax_out).sum(axis=1)
-            # loss = ((1 - args.alpha) * l_loss  + args.alpha * soft_loss).mean()
+            soft_loss = -(soft_sy * s_log_softmax_out).sum(axis=1)
+            loss = ((1 - args.alpha) * l_loss  + args.alpha * soft_loss).mean()
 
-            addi = -(s_log_softmax_out/65).sum(dim=1)
-            s_loss = ((1 - args.alpha) * l_loss  + args.alpha * addi).mean()
+            # addi = -(s_log_softmax_out/65).sum(dim=1)
+            # s_loss = ((1 - args.alpha) * l_loss  + args.alpha * addi).mean()
 
             # soft_out = F.softmax(l_out, dim=1)
             # h_loss = - torch.mean(torch.sum(soft_out * (torch.log(soft_out + 1e-5)), dim=1))
@@ -542,7 +542,7 @@ def main(args):
             # t_loss = torch.nn.CrossEntropyLoss()(t_out, ly)
 
             # loss = (s_loss + t_loss)/2
-            loss = s_loss
+            # loss = s_loss
             loss.backward()
             opt.step()
             # for param in c.parameters():
