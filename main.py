35b454eaceb169bbc0aafd365c06d6388dd6cf7f
b05902105 <b05902105@ntu.eud.tw>
2022-08-03 17:46:01 +0800

    fix bug for label correction

--- main.py
+++ main.py
@@ -532,11 +532,11 @@ def main(args):
             lx, ly = next(l_iter)
             lx, ly = lx.float().cuda(), ly.long().cuda()
             
-            sx, sy = next(s_iter)
-            sx, sy = sx.float().cuda(), sy.float().cuda()
+            # sx, sy = next(s_iter)
+            # sx, sy = sx.float().cuda(), sy.long().cuda()
 
-            # sx, sy1, sy2 = next(s_iter)
-            # sx, sy1, sy2 = sx.float().cuda(), sy1.long().cuda(), sy2.long().cuda()
+            sx, sy1, sy2 = next(s_iter)
+            sx, sy1, sy2 = sx.float().cuda(), sy1.long().cuda(), sy2.float().cuda()
             # soft_sy = class_soft_labels[sy]
             ux, _ = next(u_iter)
             ux = ux.float().cuda()
@@ -548,21 +548,21 @@ def main(args):
             # loss = criterion(s_out, sy)
             # loss = (1 - args.alpha) * criterion(s_out, sy)
             s_log_softmax_out = F.log_softmax(s_out, dim=1)
-            l_loss = nn.CrossEntropyLoss(reduction='none')(s_out, sy)
+            l_loss = nn.CrossEntropyLoss(reduction='none')(s_out, sy1)
             # l_loss2 = criterion(s_out, sy2)
 
             # s_loss = (l_loss1 + l_loss2)/2
 
             
             
-            # soft_loss = -(sy2 * s_log_softmax_out).sum(axis=1)
-            # s_loss = ((1 - args.beta) * l_loss  + args.beta * soft_loss)
+            soft_loss = -(sy2 * s_log_softmax_out).sum(axis=1)
+            s_loss = ((1 - args.beta) * l_loss  + args.beta * soft_loss)
 
             # soft_loss = -(global_soft_labels * s_log_softmax_out).sum(axis=1)
             # s_loss = ((1 - args.alpha) * s_loss  + args.alpha * soft_loss).mean()
 
-            addi = -(s_log_softmax_out/65).sum(dim=1)
-            s_loss = ((1 - args.alpha) * l_loss  + args.alpha * addi).mean()
+            # addi = -(s_log_softmax_out/65).sum(dim=1)
+            # s_loss = ((1 - args.alpha) * l_loss  + args.alpha * addi).mean()
             # s_loss = criterion(s_out, sy)
             # soft_out = F.softmax(l_out, dim=1)
             # h_loss = - torch.mean(torch.sum(soft_out * (torch.log(soft_out + 1e-5)), dim=1))
@@ -646,7 +646,7 @@ def main(args):
                 b.train()
                 c.train()
 
-        save(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/label_smoothing_{args.alpha}_{args.num_iters}.pt', f=f, b=b, c=c)
+        save(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/label_correction_{args.beta}_{args.num_iters}.pt', f=f, b=b, c=c)
         # save(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/s.pt', f=f, b=b, c=c)
 
         # output_path = Path(f'./data/{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/class_wise_label_smoothing_{args.alpha}.npz')
