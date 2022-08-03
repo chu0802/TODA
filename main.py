538847b6a16ad8b7f81bc3024bcf143f1479368a
b05902105 <b05902105@ntu.eud.tw>
2022-08-03 17:47:05 +0800

    fix bug

--- main.py
+++ main.py
@@ -490,10 +490,10 @@ def main(args):
         
         opt = torch.optim.SGD(params, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
         lr_scheduler = LR_Scheduler(opt, args.num_iters)
-        # label_correction_soft_labels = np.load(f'data/labels/label_correction_soft_labels/s{args.source}_t{args.target}_{args.dim}.npy')
-        # path = Path(args.dataset['path']) / args.dataset['domains'][args.source]
-        # s_train_dset = LabelTransformImageFolder(path, TransformNormal(train=True), label_correction_soft_labels)
-        s_train_dset = load_img_dset(args, args.source, train=train)
+        label_correction_soft_labels = np.load(f'data/labels/label_correction_soft_labels/s{args.source}_t{args.target}_{args.dim}.npy')
+        path = Path(args.dataset['path']) / args.dataset['domains'][args.source]
+        s_train_dset = LabelTransformImageFolder(path, TransformNormal(train=True), label_correction_soft_labels)
+        # s_train_dset = load_img_dset(args, args.source, train=train)
         s_train_loader = load_img_dloader(args, s_train_dset, train=True)
         s_test_dset, s_test_loader = load_img_data(args, args.source, train=False)
         
@@ -646,7 +646,7 @@ def main(args):
                 b.train()
                 c.train()
 
-        save(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/label_correction_{args.beta}_{args.num_iters}.pt', f=f, b=b, c=c)
+        save(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/label_correction_{args.beta}_{args.num_iters}_{args.dim}.pt', f=f, b=b, c=c)
         # save(f'{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/s.pt', f=f, b=b, c=c)
 
         # output_path = Path(f'./data/{args.dataset["name"]}/3shot/res34/s{args.source}_t{args.target}_{args.seed}/class_wise_label_smoothing_{args.alpha}.npz')
