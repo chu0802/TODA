0550ca7732f049f088db1adae40fc10ace573979
chu0802 <r09922104@ntu.edu.tw>
2022-08-30 10:00:29 +0800

    move all scripts to script

--- run.sh
+++
@@ -1,3 +0,0 @@
-python new_main.py --mode ssda --method base --source 1 --target 3 --seed 2025 --device 3
-python new_main.py --mode ssda --method base --source 2 --target 0 --seed 2026 --device 3
-python new_main.py --mode ssda --method base --source 2 --target 1 --seed 2027 --device 3
--- run2.sh
+++
@@ -1,4 +0,0 @@
-python new_main.py --mode ssda --method base --source 2 --target 3 --seed 2028 --device 2
-python new_main.py --mode ssda --method base --source 3 --target 0 --seed 2029 --device 2
-python new_main.py --mode ssda --method base --source 3 --target 1 --seed 2030 --device 2
-python new_main.py --mode ssda --method base --source 3 --target 2 --seed 2031 --device 2
