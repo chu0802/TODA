69cf90e14c6d366e6749ecf823193ffaaa831e24
chu0802 <r09922104@ntu.edu.tw>
2022-09-10 16:54:05 +0800

    ignore script

--- script_generator.py
+++ script_generator.py
@@ -15,19 +15,19 @@ seed_list = rng.integers(1e4, size=12)
 
 args = Namespace()
 args.num_iters = 5000
-args.method = 'MME'
-args.alpha = 0.0
-args.T = 0.0
+args.method = 'MME_LCD_PPC'
+args.alpha = 0.3
+args.T = 0.4
 args.lr = 0.02
-# args.update_interval = 500
-# args.note = 'based_on_mme'
+args.update_interval = 500
+args.note = 'based_on_mme'
 gh = GH()
 
 l = [[] for _ in range(len(device))]
 for i, (s, t) in enumerate(permutations(range(4), 2)):
     idx = i % len(device)
     args.source, args.target, args.seed = s, t, seed_list[i]
-    # args.init = gh.regSearch(f':MME/.*seed:{args.seed}.*source.{s}.target.{t}')[0]
+    args.init = gh.regSearch(f':MME/.*seed:{args.seed}.*source.{s}.target.{t}')[0]
     cmd = 'python new_main.py ' + ' '.join([f'--{k} {v}' for k, v in args.__dict__.items()]) + f' --device {device[idx]}'
     l[idx].append(cmd)
 
